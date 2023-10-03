568aef892f7fddba80b2b82ac8783c4f79080171
jgrissom <jgrissom91@gmail.com>
2023-10-03 14:05:31 -0500

    [1.08] Display Movie - Create method in Movie class to display movie - Display movie instance

--- Program.cs
+++ Program.cs
@@ -14,5 +14,6 @@ Movie movie = new Movie
   title = "Jeff's Killer Movie (2019)",
   genres = new List<string> { "Action", "Romance", "Comedy" }
 };
+Console.WriteLine(movie.Display());
 
 logger.Info("Program ended");
